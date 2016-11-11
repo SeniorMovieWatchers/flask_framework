ba77b59011923e5b0edcf4c1ea4ff5dcb3c7529a
Chris <Chris@ChisMacbook.local>
2016-11-10 23:16:49 -0600

    search

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route("/signin", methods=["POST"])
 def sign_in():
     if request.method == "POST":
@@ -16,27 +17,23 @@ def sign_in():
         email = request.json["email"]
         sql_query = "SELECT * FROM user WHERE email='%s'" % email
         cursor.execute(sql_query)
-	print (cursor.rowcount)
         if cursor.rowcount == 0:
             sql_query = "INSERT INTO user(id, username, email) values('%s', '%s', '%s')" % (id, username, email)
             cursor.execute(sql_query)
             database.commit()
     return "Unsuccessful POST request"
 
-@app.route("/search-user-email", methods=["POST"])
-def search_user_email():
-  if request.method == "POST":
-     email = request.json['email']
-     sql_query = "SELECT * FROM user WHERE email='%s'"%email
-     cursor.execute(sql_query)
-     user_list = []
-     for id, username, email, password in cursor:    
-       user = {"username" : username,
-               "email"    : email,
-               "id" : id}
-       user_list.append(user)
-     result = {"query":sql_query, "user_list":user_list}
-     return jsonify(result)
+
+@app.route("/search-movie", methods=["POST"])
+def search_movie():
+    if request.method == "POST":
+        keyword = request.json["keyword"]
+        sql_query = "SELECT * FROM movie WHERE title LIKE '%" + keyword +"%'"
+        cursor.execute(sql_query)
+        rows = cursor.fetchall()
+        print (rows)
+    return "Unsuccessful POST request"
+
 
 @app.route("/search-user-username", methods=["POST"])
 def search_user_username():
@@ -53,19 +50,5 @@ def search_user_username():
      result = {"query":sql_query, "user_list":user_list}
      return jsonify(result) 
 
-@app.route("/duplicate-email", methods=["GET"])
-def duplicate_email():
-  if request.method == "GET":
-     sql_query = "SELECT u1.username, u1.email, u1.id FROM user u1, user u2 WHERE u1.email = u2.email AND u1.id <> u2.id"
-     cursor.execute(sql_query)
-     user_list = []
-     for username, email, id in cursor:
-       user = {"username" : username,
-               "email"    : email,
-               "id" : id}
-       user_list.append(user)
-     result = {"query":sql_query, "user_list":user_list}
-     return jsonify(result)
-    
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
