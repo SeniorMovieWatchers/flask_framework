6f20896a4c6d5734d4e123855b58204d634070e0
Chris Fang <chrisfanght@gmail.com>
2016-12-04 16:34:02 -0600

    add notifications

--- static/front_page.js
+++ static/front_page.js
@@ -2,6 +2,10 @@ $("#search-movie").click(function() {
     var new_data = {
         keyword: $("#search-text").val()
     };
+
+    $.notify({
+      message: 'Hello World' 
+    });
     
     $.ajax({
             url: 'http://fa16-cs411-50.cs.illinois.edu/home/search-movie',
@@ -77,30 +81,30 @@ function addFavs(i){
 
 $("#search-user").click(function() {
     var new_data = {
-            username : $("#user").val()
-        };
+        username : $("#user").val()
+    };
 
-        $.ajax({
-            url: 'http://fa16-cs411-50.cs.illinois.edu/home/search-user-username',
-            dataType: 'JSON',
-            type: 'POST',
-            contentType: 'application/json; charset=utf-8',
-            success: function(result){
-                $("#table-content").html('');
-                $("#query-executed").html(result['query']);
-                user_list = result['user_list'];
-                var index;
-                for (index = 0; index < user_list.length; index++) {
-                    var user = user_list[index];
-                    var result = "<tr>";
-                    result += "<td>" + user['id'] + "</td>";
-                    result += "<td>" + user['username'] + "</td>"
-                    result += "<td>" + user['email'] +"</td>";
-                    $( "#table-content" ).append(result);
-                }
-            },
-            data: JSON.stringify(new_data)
-        });
+    $.ajax({
+        url: 'http://fa16-cs411-50.cs.illinois.edu/home/search-user-username',
+        dataType: 'JSON',
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        success: function(result){
+            $("#table-content").html('');
+            $("#query-executed").html(result['query']);
+            user_list = result['user_list'];
+            var index;
+            for (index = 0; index < user_list.length; index++) {
+                var user = user_list[index];
+                var result = "<tr>";
+                result += "<td>" + user['id'] + "</td>";
+                result += "<td>" + user['username'] + "</td>"
+                result += "<td>" + user['email'] +"</td>";
+                $( "#table-content" ).append(result);
+            }
+        },
+        data: JSON.stringify(new_data)
+    });
 });
 
 $("#search-user-email").click(function() {
